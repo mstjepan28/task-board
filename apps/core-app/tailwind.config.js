0a9e7d4ece8922a3f30bdba1d6fe742e5c0be2bd
Stjepan Markovčić <stjepan.markovcic@superius.hr>
2023-11-29 18:15:38 +0100

    cleanup

---
+++ apps/core-app/tailwind.config.js
@@ -0,0 +1,6 @@
+import { theme, content } from "../../tailwind.config.js";
+
+export default {
+  content: content,
+  theme: theme,
+};
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export const theme = {
+  extend: {
+    // colors: {
+    //   primary: {},
+    // },
+  },
+};
+
+export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx,mdx}", "../../shared/**/src/**/*.{js,ts,jsx,tsx,mdx}"];
