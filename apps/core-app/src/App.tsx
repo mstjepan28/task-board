463c2658bcc31b027d946292c352eadf3c2c53e6
markovcics@gmail.com <mstjepan28>
2024-01-20 09:42:23 +0100

    test commit

--- apps/core-app/src/App.tsx
+++ apps/core-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { Chat } from "@shared/live-chat";
 
 export const App = () => {
+  // test
   return (
     <div className="h-[100svh] w-full flex items-center justify-center bg-gray-500">
       <Chat />
