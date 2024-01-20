b493296c00f0b4d96a63c1f75f0c1a85bca0c9c5
mstjepan28 <markovcics@gmail.com>
2024-01-20 10:05:49 +0100

    future project - text editor workspace

--- apps/core-app/src/App.tsx
+++ apps/core-app/src/App.tsx
@@ -1,7 +1,6 @@
 import { Chat } from "@shared/live-chat";
 
 export const App = () => {
-  // test1
   return (
     <div className="h-[100svh] w-full flex items-center justify-center bg-gray-500">
       <Chat />
