1af02709759dd51abd8bf7fb2f615111f59ef5eb
mstjepan28 <markovcics@gmail.com>
2024-01-21 10:47:25 +0100

    draw logic for ball and bounce board

--- shared/brick-breaker/src/screens/BrickBreaker.tsx
+++ shared/brick-breaker/src/screens/BrickBreaker.tsx
@@ -1,35 +1,67 @@
-import { useLayoutEffect, useRef } from "react";
-import { getCanvasSize, getContext } from "../utils/canvasHelpers";
+import { useLayoutEffect, useMemo, useRef } from "react";
+import { drawBall, drawBoard, getCanvasSize, getContext } from "../utils/canvasHelpers";
 
 export const BrickBreaker = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  const draw = (context: CanvasRenderingContext2D | null) => {
+  const canvasSize = useMemo(() => {
+    const { innerHeight, innerWidth } = window;
+    return {
+      width: innerWidth * 0.8,
+      height: innerHeight * 0.8,
+    };
+  }, []);
+
+  // --- Handle drawing the bounce board ---
+
+  const handleKeyPress = (key: string) => {
+    const context = getContext(canvasRef);
+    if (!context) {
+      return;
+    }
+
+    const MOVE_BY = 1;
+
+    switch (key) {
+      case "ArrowLeft":
+        drawBoard(context, -MOVE_BY);
+        break;
+      case "ArrowRight":
+        drawBoard(context, MOVE_BY);
+        break;
+      default:
+        break;
+    }
+  };
+
+  // --- Main draw loop ---
+
+  const mainDrawLoop = (context: CanvasRenderingContext2D | null) => {
     if (!context) {
       return;
     }
 
     const size = getCanvasSize(context);
+    context.clearRect(0, 0, size.width, size.height);
 
-    const width = size.width * 0.1;
-    const height = size.height * 0.1;
-
-    const xPos = size.width * 0.5 - 25;
-    const yPos = size.height - height;
-
-    console.log(xPos, yPos, width, height);
-
-    context.fillStyle = "#fff";
-    context.fillRect(xPos, yPos, width, height);
+    drawBall(context, size.centerX, size.centerY);
+    drawBoard(context, 0);
   };
 
   useLayoutEffect(() => {
     const context = getContext(canvasRef);
-    draw(context);
+    setInterval(() => mainDrawLoop(context), 1000);
   }, []);
 
   return (
-    <canvas ref={canvasRef} className="w-10/12 h-10/12 bg-gray-950 rounded-xl p-4">
+    <canvas
+      tabIndex={0}
+      ref={canvasRef}
+      onKeyDownCapture={(event) => handleKeyPress(event.key)}
+      width={canvasSize.width}
+      height={canvasSize.height}
+      className="bg-gray-950 rounded-xl p-4 focus:outline-none"
+    >
       <p>Sorry, your browser does not support canvas.</p>
     </canvas>
   );
--- shared/brick-breaker/src/utils/canvasHelpers.tsx
+++ shared/brick-breaker/src/utils/canvasHelpers.tsx
@@ -15,6 +15,7 @@ export const getContext = (canvasRef: RefObject<HTMLCanvasElement>) => {
     throw new Error("Canvas context not found");
   }
 
+  context.imageSmoothingEnabled = false;
   return context;
 };
 
@@ -23,11 +24,44 @@ export const getCanvasSize = (context: CanvasRenderingContext2D | null) => {
     return {
       width: 0,
       height: 0,
+      centerX: 0,
+      centerY: 0,
     };
   }
 
   return {
     width: context.canvas.width,
     height: context.canvas.height,
+    centerX: context.canvas.width / 2,
+    centerY: context.canvas.height / 2,
   };
+};
+
+export const drawBall = (context: CanvasRenderingContext2D, x: number, y: number) => {
+  context.beginPath();
+
+  context.arc(x, y, 10, 0, Math.PI * 2);
+  context.fillStyle = "#0095DD";
+  context.fill();
+
+  context.closePath();
+};
+
+export const drawBoard = (context: CanvasRenderingContext2D | null, xOffset: number) => {
+  if (!context) {
+    return;
+  }
+
+  const size = getCanvasSize(context);
+  context.imageSmoothingEnabled = false;
+
+  const width = size.width * 0.125;
+  const height = size.height * 0.025;
+
+  // init position in the middle of the screen and add offset
+  const xPos = size.centerX - width / 2 + xOffset;
+  const yPos = size.height - height;
+
+  context.fillStyle = "#fff";
+  context.fillRect(xPos, yPos, width, height);
 };
