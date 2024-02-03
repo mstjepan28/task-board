028c86391f5ba61b8c332038dc81fbd41692efc9
mstjepan28 <markovcics@gmail.com>
2024-02-03 11:22:50 +0100

    navigation on bottom

--- apps/core-app/src/layout/NavigationLayout.tsx
+++ apps/core-app/src/layout/NavigationLayout.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { NavLink } from "../components/NavLink";
 
 export const NavigationLayout = () => {
   return (
     <>
-      <div className="h-[100svh] w-full items-center justify-center bg-gray-800">
-        <div className="px-4 py-2 flex gap-x-4 text-white border-b mb-4">
+      <div className="h-full w-full flex flex-col bg-gray-800">
+        <div className="basis-full">
+          <Outlet />
+        </div>
+
+        <div className="px-4 py-2 flex gap-x-4 text-white border-t mt-4">
           <NavLink to="/">Home</NavLink>
           <NavLink to="/task-board">Task board</NavLink>
           <NavLink to="/brick-breaker">Brick breaker</NavLink>
@@ -14,13 +17,7 @@ export const NavigationLayout = () => {
           <NavLink to="/live-chat">Live chat</NavLink>
           <NavLink to="/sudoku">Sudoku</NavLink>
         </div>
-
-        <div>
-          <Outlet />
-        </div>
       </div>
-
-      <TanStackRouterDevtools />
     </>
   );
 };
