ed688f021b61575a4a62390f1171add971e9ac7a
mstjepan28 <markovcics@gmail.com>
2024-02-03 15:47:18 +0100

    render grouped items

--- shared/todo-app/src/components/TaskBoard.tsx
+++ shared/todo-app/src/components/TaskBoard.tsx
@@ -1,12 +1,18 @@
+import { useTaskList } from "../hooks/useTaskList";
 import { TaskColumn } from "./TaskColumn";
 
 export const TaskBoard = () => {
+  const { groupedTaskList } = useTaskList();
+  const { pending, in_progress, canceled, failed, completed } = groupedTaskList;
+
   return (
     <div className="h-full flex flex-col">
       <div className="flex basis-full gap-x-4 mx-8 my-4">
-        <TaskColumn title="To do" taskList={[]} />
-        <TaskColumn title="In progress" taskList={[]} />
-        <TaskColumn title="Done" taskList={[]} />
+        <TaskColumn title="Pending" taskList={pending} />
+        <TaskColumn title="In progress" taskList={in_progress} />
+        <TaskColumn title="Canceled" taskList={canceled} />
+        <TaskColumn title="Failed" taskList={failed} />
+        <TaskColumn title="Completed" taskList={completed} />
       </div>
     </div>
   );
