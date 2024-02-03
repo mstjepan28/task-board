77ef179fcfa19ed4c08a955e3542a183cef695bc
mstjepan28 <markovcics@gmail.com>
2024-02-03 14:06:15 +0100

    navigation layout height sizing

--- apps/core-app/src/layout/NavigationLayout.tsx
+++ apps/core-app/src/layout/NavigationLayout.tsx
@@ -3,21 +3,19 @@ import { NavLink } from "../components/NavLink";
 
 export const NavigationLayout = () => {
   return (
-    <>
-      <div className="h-full w-full flex flex-col bg-gray-800">
-        <div className="basis-full">
-          <Outlet />
-        </div>
-
-        <div className="px-4 py-2 flex gap-x-4 text-white border-t mt-4">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/task-board">Task board</NavLink>
-          <NavLink to="/brick-breaker">Brick breaker</NavLink>
-          <NavLink to="/cryptogram">Cryptogram</NavLink>
-          <NavLink to="/live-chat">Live chat</NavLink>
-          <NavLink to="/sudoku">Sudoku</NavLink>
-        </div>
+    <div className="h-full flex flex-col bg-gray-800">
+      <div className="basis-full max-h-full overflow-y-scroll ">
+        <Outlet />
       </div>
-    </>
+
+      <div className="px-4 py-2 flex gap-x-4 text-white border-t">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/task-board">Task board</NavLink>
+        <NavLink to="/brick-breaker">Brick breaker</NavLink>
+        <NavLink to="/cryptogram">Cryptogram</NavLink>
+        <NavLink to="/live-chat">Live chat</NavLink>
+        <NavLink to="/sudoku">Sudoku</NavLink>
+      </div>
+    </div>
   );
 };
