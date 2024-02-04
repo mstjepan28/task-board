1eb8ddd4dd8724bf07db4b57c12b7f792df3b6ce
mstjepan28 <markovcics@gmail.com>
2024-02-04 18:12:57 +0100

    chess board

--- shared/chess/src/screens/ChessScreen.tsx
+++ shared/chess/src/screens/ChessScreen.tsx
@@ -1,7 +1,34 @@
+import { useMemo } from "react";
+
+interface IProps {
+  className?: string;
+}
+
+const BoardSquare = ({ className }: IProps) => {
+  return <div className={`w-16 aspect-square flex justify-center items-center ${className}`}>1</div>;
+};
+
 export const ChessScreen = () => {
+  const board = useMemo(() => {
+    const board = [];
+
+    for (let i = 0; i < 8; i++) {
+      const row = [];
+      for (let j = 0; j < 8; j++) {
+        const bgColor = (i + j) % 2 === 1 ? "bg-gray-900" : "bg-gray-50";
+        const square = <BoardSquare key={`${i}-${j}`} className={bgColor} />;
+
+        row.push(square);
+      }
+      board.push(row);
+    }
+
+    return board;
+  }, []);
+
   return (
-    <div>
-      <h1>Chess</h1>
+    <div className="h-full flex justify-center items-center">
+      <div className="w-fit grid grid-cols-8 border-white border-2">{board}</div>
     </div>
   );
 };
