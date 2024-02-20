b05117ff53ba2084b3c9b9f3d04a1dcc4bacd23f
mstjepan28 <markovcics@gmail.com>
2024-02-20 20:04:40 +0100

    type fixes - chat service

--- services/chat-server/src/index.ts
+++ services/chat-server/src/index.ts
@@ -2,31 +2,23 @@
 Bun.serve({
   port: 8080,
   fetch(req, server) {
-    // upgrade the request to a WebSocket
     if (server.upgrade(req)) {
-      return; // do not return a Response
+      return;
     }
     return new Response("Upgrade failed :(", { status: 500 });
   },
   websocket: {
-    // a message is received
     message(ws, message) {
       console.log("WebSocket message received:", message);
       ws.send(message);
     },
-
-    // a socket is opened
-    open(ws) {
+    open() {
       console.log("WebSocket opened");
     },
-
-    // a socket is closed
-    close(ws, code, message) {
+    close(_, code, message) {
       console.log("WebSocket closed:", code, message);
     },
-
-    // the socket is ready to receive more data
-    drain(ws) {
+    drain() {
       console.log("WebSocket can receive more data");
     },
   },
