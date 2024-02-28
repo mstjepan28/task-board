0ad1e72772d1b9e8c94ef31f69c5d88a1cae109c
mstjepan28 <markovcics@gmail.com>
2024-02-28 20:00:28 +0100

    delete drop zone wip

--- shared/todo-app/src/components/DeleteButton.tsx
+++ shared/todo-app/src/components/DeleteButton.tsx
@@ -1,38 +1,39 @@
-import { debounce, throttle } from "@services/utils";
-import { useRef } from "react";
+import { throttle } from "@services/utils";
+import { useRef, DragEvent } from "react";
 
 export const DeleteButton = () => {
   const deleteZoneRef = useRef<HTMLDivElement>(null);
 
-  const handleDragOver = throttle(250, () => {
+  const expandDeleteZone = throttle(250, (event: DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+
     deleteZoneRef.current?.classList.remove("w-0");
     deleteZoneRef.current?.classList.add("w-36");
-
-    debounce(1000, () => {
-      console.log("deleting");
-    });
   });
 
-  // const onDragOverDeleteZone = () => {
-  //   deleteZoneRef.current?.classList.add("w-0");
-  //   deleteZoneRef.current?.classList.remove("w-36");
-  // };
+  const hideDeleteZone = (event: any) => {
+    console.log("leave");
+    event.preventDefault();
+
+    deleteZoneRef.current?.classList.add("w-0");
+    deleteZoneRef.current?.classList.remove("w-36");
+  };
 
   return (
-    <div
-      onDragOver={handleDragOver}
-      className="relative border rounded-lg text-white px-2 py-1 transition-all"
-    >
-      <span>Delete</span>
+    <div onDragEnter={expandDeleteZone} onDragLeave={hideDeleteZone} className="relative bg-blue-600">
+      <div className="border rounded-lg text-white px-2 py-1">
+        <span>Delete</span>
+      </div>
 
       <div
         ref={deleteZoneRef}
-        onDrop={() => console.log("dropped")}
-        className="w-0 flex justify-center items-center overflow-hidden aspect-square z-10 absolute top-0 right-0 bg-red-600 border rounded-lg transition-all duration-1000"
+        className="
+          w-0 flex justify-center items-center overflow-hidden 
+          aspect-square z-10 absolute top-0 right-0 bg-red-600 
+          rounded-lg transition-all duration-1000
+        "
       >
-        <span className="whitespace-nowrap text-white text-xs">
-          Drop here to delete
-        </span>
+        <span className="whitespace-nowrap text-white text-xs">Drop here to delete</span>
       </div>
     </div>
   );
