c3f40ff54448ec11471a0db2fa46924cf9007931
mstjepan28 <markovcics@gmail.com>
2024-02-28 20:26:50 +0100

    opening delete zone fixes

--- shared/todo-app/src/components/DeleteButton.tsx
+++ shared/todo-app/src/components/DeleteButton.tsx
@@ -20,20 +20,28 @@ export const DeleteButton = () => {
   };
 
   return (
-    <div onDragEnter={expandDeleteZone} onDragLeave={hideDeleteZone} className="relative bg-blue-600">
-      <div className="border rounded-lg text-white px-2 py-1">
-        <span>Delete</span>
-      </div>
+    <div className="relative flex">
+      <span className="invisible px-2 max-h-0">Delete</span>
 
       <div
-        ref={deleteZoneRef}
-        className="
-          w-0 flex justify-center items-center overflow-hidden 
-          aspect-square z-10 absolute top-0 right-0 bg-red-600 
-          rounded-lg transition-all duration-1000
-        "
+        onDragEnter={expandDeleteZone}
+        onDragLeave={hideDeleteZone}
+        className="z-10 top-0 right-0 absolute flex flex-col items-end"
       >
-        <span className="whitespace-nowrap text-white text-xs">Drop here to delete</span>
+        <div className="ml-auto w-fit border rounded-lg text-white px-2 py-1">
+          <span>Delete</span>
+        </div>
+
+        <div
+          ref={deleteZoneRef}
+          className="
+          w-0 flex justify-center items-center overflow-hidden 
+          aspect-square bg-red-600 rounded-lg transition-all 
+          duration-1000 pointer-events-none -top-[34px] relative
+        "
+        >
+          <span className="whitespace-nowrap text-white text-xs">Drop here to delete</span>
+        </div>
       </div>
     </div>
   );
