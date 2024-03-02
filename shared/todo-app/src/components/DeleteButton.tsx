91d4a991a2607069c38b8e24db9df9c448dfbb1c
mstjepan28 <markovcics@gmail.com>
2024-03-02 11:47:16 +0100

    delete zone fixes

--- shared/todo-app/src/components/DeleteButton.tsx
+++ shared/todo-app/src/components/DeleteButton.tsx
@@ -1,44 +1,42 @@
-import { throttle } from "@services/utils";
-import { useRef, DragEvent } from "react";
+import { DragEvent, useRef } from "react";
 
 export const DeleteButton = () => {
   const deleteZoneRef = useRef<HTMLDivElement>(null);
 
-  const expandDeleteZone = throttle(250, (event: DragEvent<HTMLDivElement>) => {
+  const expandDeleteZone = (event: DragEvent<HTMLDivElement>) => {
     event.preventDefault();
 
     deleteZoneRef.current?.classList.remove("w-0");
     deleteZoneRef.current?.classList.add("w-36");
-  });
+  };
 
-  const hideDeleteZone = (event: any) => {
-    console.log("leave");
-    event.preventDefault();
-
+  const hideDeleteZone = () => {
     deleteZoneRef.current?.classList.add("w-0");
     deleteZoneRef.current?.classList.remove("w-36");
   };
 
+  const deleteItem = () => {
+    console.log("implement delete");
+    hideDeleteZone();
+  };
+
   return (
-    <div className="relative flex">
+    <div className="relative flex" onDragOver={expandDeleteZone} onDragLeave={hideDeleteZone}>
       <span className="invisible px-2 max-h-0">Delete</span>
 
-      <div
-        onDragEnter={expandDeleteZone}
-        onDragLeave={hideDeleteZone}
-        className="z-10 top-0 right-0 absolute flex flex-col items-end"
-      >
+      <div className="z-10 top-0 right-0 absolute flex flex-col items-end">
         <div className="ml-auto w-fit border rounded-lg text-white px-2 py-1">
           <span>Delete</span>
         </div>
 
         <div
           ref={deleteZoneRef}
+          onDrop={deleteItem}
           className="
-          w-0 flex justify-center items-center overflow-hidden 
-          aspect-square bg-red-600 rounded-lg transition-all 
-          duration-1000 pointer-events-none -top-[34px] relative
-        "
+            w-0 flex justify-center items-center overflow-hidden 
+            aspect-square bg-red-600 rounded-lg transition-all 
+            duration-1000 top-1 relative
+          "
         >
           <span className="whitespace-nowrap text-white text-xs">Drop here to delete</span>
         </div>
