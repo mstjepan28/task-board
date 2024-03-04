c950a1a6b44980684a2d93ba652def751d198837
mstjepan28 <markovcics@gmail.com>
2024-03-04 18:15:20 +0100

    code refactor fixes

--- shared/todo-app/src/components/TaskColumn.tsx
+++ shared/todo-app/src/components/TaskColumn.tsx
@@ -1,8 +1,9 @@
-import { DragEvent, useContext, useMemo } from "react";
+import { useContext, useMemo } from "react";
 import { TaskListContext } from "../context/TaskListContext";
+import { TTaskStatus, TaskStatus } from "../enums/taskStatus";
 import { TTask } from "../types/task";
 import { TaskCard } from "./TaskCard";
-import { TTaskStatus, TaskStatus } from "../enums/taskStatus";
+import { getColumnId } from "../utils/helpers";
 
 interface IProps {
   status: TTaskStatus;
@@ -11,7 +12,7 @@ interface IProps {
 }
 
 export const TaskColumn = ({ status, taskList, onTaskClick }: IProps) => {
-  const { dropTask, dragTaskOver, getColumnId } = useContext(TaskListContext);
+  const { dropTask, dragTaskOver } = useContext(TaskListContext);
   const columnId = getColumnId(status);
 
   const title = useMemo(() => {
@@ -23,14 +24,6 @@ export const TaskColumn = ({ status, taskList, onTaskClick }: IProps) => {
       [TaskStatus.COMPLETED]: "Completed",
     }[status];
   }, []);
-
-  const onDrop = () => {
-    dropTask(status);
-  };
-
-  const onDragOver = (event: DragEvent<HTMLDivElement>) => {
-    dragTaskOver(event, status);
-  };
 
   const renderTaskList = () => {
     return taskList.map((task) => {
@@ -46,7 +39,12 @@ export const TaskColumn = ({ status, taskList, onTaskClick }: IProps) => {
     <div className="basis-full flex flex-col rounded-md border border-white">
       <div className="text-white font-semibold border-b px-4 py-2">{title}</div>
 
-      <div id={columnId} onDrop={onDrop} onDragOver={onDragOver} className="basis-full">
+      <div
+        id={columnId}
+        className="basis-full"
+        onDrop={() => dropTask(status)}
+        onDragOver={dragTaskOver}
+      >
         {renderTaskList()}
       </div>
     </div>
