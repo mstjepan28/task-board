f63177b7b05a86bacfb7b57692acb8be2deded74
mstjepan28 <markovcics@gmail.com>
2024-03-07 20:58:23 +0100

    added useArrayState

---
+++ services/utils/src/custom-hooks/useArrayState.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export const useArrayState = <T,>(initialState: T[]) => {
+  const [state, setState] = useState(initialState);
+
+  const push = (value: T) => {
+    setState((prev) => [...prev, value]);
+  };
+
+  const pop = () => {
+    setState((prev) => prev.slice(0, -1));
+  };
+
+  const remove = (index: number) => {
+    setState((prev) => prev.filter((_, i) => i !== index));
+  };
+
+  const clear = () => {
+    setState([]);
+  };
+
+  return { state, push, remove, pop, clear };
+};
