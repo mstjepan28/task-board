2c8c1cf54bd2bd1c62ac629c26e2ba5b9d89fe03
mstjepan28 <markovcics@gmail.com>
2024-03-08 19:34:07 +0100

    action button styles

--- shared/sudoku/src/components/ActionButton.tsx
+++ shared/sudoku/src/components/ActionButton.tsx
@@ -5,7 +5,11 @@ interface IProps {
 
 export const ActionButton = ({ label, onClick }: IProps) => {
   return (
-    <button type="button" onClick={onClick} className="flex items-center py-1">
+    <button
+      type="button"
+      onClick={onClick}
+      className="flex items-center py-1 cursor-pointer select-none"
+    >
       <span className="text-xs text-white/25">{label}</span>
     </button>
   );
