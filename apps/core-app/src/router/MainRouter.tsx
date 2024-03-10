6a231c6c3cf544f95afb1de5b092227f49926d65
mstjepan28 <markovcics@gmail.com>
2024-03-10 13:40:26 +0100

    version fixes

--- apps/core-app/src/router/MainRouter.tsx
+++ apps/core-app/src/router/MainRouter.tsx
@@ -1,7 +1,7 @@
 import { createRootRoute, createRoute, createRouter } from "@tanstack/react-router";
 import { NavigationLayout } from "../layout/NavigationLayout";
-import * as todoModule from "@shared/todo-app";
 import * as sudokuModule from "@shared/sudoku";
+import * as todoModule from "@shared/todo-app";
 import * as chatModule from "@shared/live-chat";
 import * as cryptogramModule from "@shared/cryptogram";
 
@@ -15,9 +15,9 @@ export const createMainRouter = () => {
     component: () => <></>,
   });
 
+  const chatRouter = chatModule.createModuleRouter<TRootRoute>(rootRoute);
   const todoRouter = todoModule.createModuleRouter<TRootRoute>(rootRoute);
   const sudokuRouter = sudokuModule.createModuleRouter<TRootRoute>(rootRoute);
-  const chatRouter = chatModule.createModuleRouter<TRootRoute>(rootRoute);
   const cryptogramRouter = cryptogramModule.createModuleRouter<TRootRoute>(rootRoute);
 
   const routerGroup = [indexRoute, todoRouter, sudokuRouter, chatRouter, cryptogramRouter].flat();
