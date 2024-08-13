a7d9b5b0b1cd5cf7292e869cfc89c0b70c95d5b6
mstjepan28 <markovcics@gmail.com>
2024-08-13 18:03:12 +0200

    run app on default port

--- apps/core-app/vite.config.ts
+++ apps/core-app/vite.config.ts
@@ -5,27 +5,37 @@ import { defineConfig } from "vite";
 import { qrcode } from "vite-plugin-qrcode";
 
 const mobile = !!/android|ios/.exec(process.env.TAURI_ENV_PLATFORM);
+const USE_BASIC_CONFIG = true;
 
 export default defineConfig(async () => {
   return defineConfig({
     plugins: [react(), tailwindcss(), qrcode()],
-    server: {
-      port: 1420,
-      strictPort: true,
-      host: mobile ? "0.0.0.0" : true,
-      hmr: mobile
-        ? {
-            protocol: "ws",
-            host: await internalIpV4(),
-            port: 1421,
-          }
-        : undefined,
-      watch: {
-        ignored: ["**/src-tauri/**"],
-      },
-    },
-    build: {
-      sourcemap: true,
-    },
+    server: USE_BASIC_CONFIG ? getBasicConfig() : await getTauriConfig(),
+    build: { sourcemap: true },
   });
 });
+
+const getBasicConfig = () => {
+  return {
+    port: 5173,
+    host: true,
+  };
+};
+
+const getTauriConfig = async () => {
+  return {
+    port: 1420,
+    strictPort: true,
+    host: mobile ? "0.0.0.0" : true,
+    hmr: mobile
+      ? {
+          protocol: "ws",
+          host: await internalIpV4(),
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      ignored: ["**/src-tauri/**"],
+    },
+  };
+};
