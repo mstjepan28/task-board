618b117aa2a5feb10d3277c06bcb3364cc32e191
mstjepan28 <markovcics@gmail.com>
2024-08-15 16:54:49 +0200

    basic display of tasks on screen

--- shared/task-mate/src/screens/TaskListScreen.tsx
+++ shared/task-mate/src/screens/TaskListScreen.tsx
@@ -1,3 +1,18 @@
+import { LoadingIndicator } from "@services/ui";
+import { useTaskList } from "../hooks/useTaskList";
+
 export const TaskListScreen = () => {
-  return <>TaskListScreen</>;
+  const taskList = useTaskList();
+
+  if (taskList.loading.fetching) {
+    return <LoadingIndicator size="xl" />;
+  }
+
+  return (
+    <div>
+      {taskList.dataList.map((task) => (
+        <div key={task.id}>{task.description}</div>
+      ))}
+    </div>
+  );
 };
