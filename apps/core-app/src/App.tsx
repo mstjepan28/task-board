6bfd57c1c3b5d3bd6b0878df486709686adfc81a
mstjepan28 <markovcics@gmail.com>
2024-08-15 16:51:49 +0200

    added query provider

--- apps/core-app/src/App.tsx
+++ apps/core-app/src/App.tsx
@@ -6,24 +6,38 @@ import { usePathFinderRouter } from "@shared/path-finder";
 import { useSudokuRouter } from "@shared/sudoku";
 import { useTaskMateRouter } from "@shared/task-mate";
 import { useTodoRouter } from "@shared/todo-app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const App = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+        structuralSharing: false,
+        gcTime: 60 * 1000 * 5,
+        staleTime: 60 * 1000 * 4,
+      },
+    },
+  });
+
   return (
-    <RouterProvider
-      router={createBrowserRouter([
-        { path: "/", element: <Navigate to="/sudoku" replace /> },
-        ...useNumberGuesserRouter(),
-        ...usePathFinderRouter(),
-        ...useCryptogramRouter(),
-        ...useTaskMateRouter(),
-        ...useSudokuRouter(),
-        ...useChatRouter(),
-        ...useTodoRouter(),
-        {
-          path: "*",
-          element: <div className="h-screen flex justify-center items-center">404 - Page not found</div>,
-        },
-      ])}
-    />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider
+        router={createBrowserRouter([
+          { path: "/", element: <Navigate to="/sudoku" replace /> },
+          ...useNumberGuesserRouter(),
+          ...usePathFinderRouter(),
+          ...useCryptogramRouter(),
+          ...useTaskMateRouter(),
+          ...useSudokuRouter(),
+          ...useChatRouter(),
+          ...useTodoRouter(),
+          {
+            path: "*",
+            element: <div className="h-screen flex justify-center items-center">404 - Page not found</div>,
+          },
+        ])}
+      />
+    </QueryClientProvider>
   );
 };
