0f1c31e00103113de3a8d338b6fc61cb5644ed7d
mstjepan28 <markovcics@gmail.com>
2024-08-15 17:09:16 +0200

    taskList filtering by search

--- shared/task-mate/src/screens/TaskListScreen.tsx
+++ shared/task-mate/src/screens/TaskListScreen.tsx
@@ -1,18 +1,25 @@
-import { LoadingIndicator } from "@services/ui";
+import { LoadingIndicator, Searchbar } from "@services/ui";
 import { useTaskList } from "../hooks/useTaskList";
+import { TaskCard } from "../components/TaskCard";
 
 export const TaskListScreen = () => {
   const taskList = useTaskList();
 
-  if (taskList.loading.fetching) {
-    return <LoadingIndicator size="xl" />;
-  }
-
   return (
-    <div>
-      {taskList.dataList.map((task) => (
-        <div key={task.id}>{task.description}</div>
-      ))}
+    <div className="h-full flex flex-col px-2 py-4">
+      <div className="pb-4">
+        <Searchbar searchValue={taskList.filters.search} searchFn={taskList.search} />
+      </div>
+
+      {taskList.loading.fetching ? (
+        <LoadingIndicator size="xl" />
+      ) : (
+        <div className="basis-full flex flex-col gap-y-4">
+          {taskList.dataList.map((task, index) => {
+            return <TaskCard key={task.id + index} task={task} />;
+          })}
+        </div>
+      )}
     </div>
   );
 };
