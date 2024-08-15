b5f8152dca443e0898ba6253a3bbbd4a318c4177
mstjepan28 <markovcics@gmail.com>
2024-08-15 17:08:40 +0200

    searchbar

--- services/ui/src/components/Searchbar.tsx
+++ services/ui/src/components/Searchbar.tsx
@@ -1,13 +1,44 @@
-import { twMerge } from "tailwind-merge";
+import { useRef } from "react";
+import { HiSearch, HiX } from "react-icons/hi";
 
 interface IProps {
-  className?: string | undefined;
+  searchValue?: string | undefined;
+  searchFn: (query: string) => void;
 }
 
-export const Searchbar = ({ className }: IProps) => {
+export const Searchbar = ({ searchValue, searchFn }: IProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const clearInput = () => {
+    if (!inputRef || !inputRef.current) {
+      console.error("No search input found");
+      return;
+    }
+
+    inputRef.current.value = "";
+    searchFn("");
+  };
+
   return (
-    <div className={twMerge("w-full rounded-lg overflow-hidden", className)}>
-      <input type="text" className="w-full bg-gray-100  px-4 py-1 outline-none hover:uppercase rounded-lg" />
+    <div
+      className="
+        flex items-center gap-x-2.5 rounded-lg border border-gray-300 bg-gray-50 px-3
+        focus-within:border-primary-600 focus-within:ring-primary-600
+      "
+    >
+      <HiSearch size="16" className="text-gray-600" />
+
+      <input
+        ref={inputRef}
+        placeholder="Search..."
+        defaultValue={searchValue}
+        onChange={({ target }) => searchFn(target.value)}
+        className="peer w-full bg-transparent py-2 border-none placeholder:text-gray-500 focus:outline-none"
+      />
+
+      <button type="button" onClick={clearInput} className="peer-placeholder-shown:hidden">
+        <HiX size="16" className="text-gray-600" />
+      </button>
     </div>
   );
 };
